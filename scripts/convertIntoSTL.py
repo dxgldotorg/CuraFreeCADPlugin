a97407c476b0f18d7d40cf749cdb15d5c4c7d350
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2018-04-25 23:04:33 +0200

    Simplify export macro
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com> (github: thopiekar)

--- scripts/convertIntoSTL.py
+++ scripts/convertIntoSTL.py
@@ -2,21 +2,24 @@ import os
 import sys
 
 import FreeCAD
+import FreeCADGui
 import Part
 import Mesh
 
 in_fn, out_fn = sys.argv[-2:]
 
+print("OPENING: {}".format(in_fn))
+FreeCADGui.setupWithoutGUI()
 part_object = FreeCAD.open(in_fn)
 
-all_objs_names = part_object.Objects
 __objs__ = []
 
-for obj_name in all_objs_names:
+for obj_name in part_object.Objects:
     if obj_name.ViewObject:
         if obj_name.ViewObject.isVisible():
             __objs__.append(obj_name)
     else: # <Part::PartFeature>
         __objs__.append(obj_name)
 
+print("EXPORTING: {}".format(out_fn))
 Mesh.export(__objs__, out_fn)
