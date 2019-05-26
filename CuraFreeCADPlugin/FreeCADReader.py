3b118e3154e4c9a8f1d98d79f2fd24b66f98027c
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2019-05-26 18:27:23 +0200

    Cleanups and codestyle
    Signed-off-by: Thomas Karl Pietrowski <thopiekar@gmail.com> (github: thopiekar)

--- CuraFreeCADPlugin/FreeCADReader.py
+++ CuraFreeCADPlugin/FreeCADReader.py
@@ -5,26 +5,27 @@ import os
 import platform
 
 # Uranium
-from UM.Application import Application # @UnresolvedImport
-from UM.Logger import Logger # @UnresolvedImport
-from UM.i18n import i18nCatalog # @UnresolvedImport
-from UM.Version import Version # @UnresolvedImport
+from UM.Application import Application  # @UnresolvedImport
+from UM.Logger import Logger  # @UnresolvedImport
+from UM.i18n import i18nCatalog  # @UnresolvedImport
+from UM.Version import Version  # @UnresolvedImport
 
 # Since 3.4: Register Mimetypes:
 if Version("3.4") <= Version(Application.getInstance().getVersion()):
     from UM.MimeTypeDatabase import MimeTypeDatabase, MimeType
 
-# CadIntegrationUtils
-from .CadIntegrationUtils.CommonCLIReader import CommonCLIReader # @UnresolvedImport
+# CIU
+from .CadIntegrationUtils.CommonCLIReader import CommonCLIReader  # @UnresolvedImport
 
 i18n_catalog = i18nCatalog("FreeCADPlugin")
+
 
 class FreeCADReader(CommonCLIReader):
     def __init__(self):
         super().__init__()
 
         if Version("3.4") <= Version(Application.getInstance().getVersion()):
-            MimeTypeDatabase.addMimeType(MimeType(name = "application/x-extension-fcstd",
+            MimeTypeDatabase.addMimeType(MimeType(name="application/x-extension-fcstd",
                                                   comment="FreeCAD files",
                                                   suffixes=["fcstd"]
                                                   )
@@ -42,7 +43,7 @@ class FreeCADReader(CommonCLIReader):
 
         return super().openForeignFile(options)
 
-    def exportFileAs(self, options, quality_enum = None):
+    def exportFileAs(self, options, quality_enum=None):
         Logger.log("d", "Exporting file: %s", options["tempFile"])
 
         cli = 'FreeCADCmd'
@@ -58,14 +59,18 @@ class FreeCADReader(CommonCLIReader):
                "--stl",
                options["tempFile"],
                ]
+
+        cwd = os.path.split(options["foreignFile"])[0]
+
         try:
             ret = self.executeCommand([cli, ] + opt,
-                                      cwd = os.path.split(options["foreignFile"])[0],
+                                      cwd=cwd,
                                       )
         except:
-            cli = cli.lower() # Ubuntu: Command name is since bioic in lowercase.
+            # Ubuntu: Command name is since bioic in lowercase.
+            cli = cli.lower()
             ret = self.executeCommand([cli, ] + opt,
-                                      cwd = os.path.split(options["foreignFile"])[0],
+                                      cwd=cwd,
                                       )
         if ret != 0:
             Logger.log("c", "Returncode is not 0!")
